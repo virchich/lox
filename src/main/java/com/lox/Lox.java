d9cdfccace5f0c677654ce05ae75504d372b43b5
virchich <68477339+virchich@users.noreply.github.com>
2025-03-01 12:25:10 -0500

    chore: Add and use Interpreter

--- src/main/java/com/lox/Lox.java
+++ src/main/java/com/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -27,6 +29,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -52,7 +55,7 @@ public class Lox {
 
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expr));
+        interpreter.interpret(expr);
     }
 
     static void error(int line, String message) {
@@ -68,5 +71,10 @@ public class Lox {
     static void error(Token token, String message) {
         if (token.type == TokenType.EOF) report(token.line, " at end: ", message);
         else report(token.line, " at '" + token.lexeme, "': " + message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
