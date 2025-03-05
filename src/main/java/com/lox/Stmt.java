0ec76ba87f84cc1696aca2e5a8aebf86960a0120
virchich <68477339+virchich@users.noreply.github.com>
2025-03-04 19:18:42 -0500

    chore: Remove unused import

--- src/main/java/com/lox/Stmt.java
+++ src/main/java/com/lox/Stmt.java
@@ -1,51 +1,54 @@
 package com.lox;
 
-import java.util.List;
-
 public abstract class Stmt {
+    abstract <R> R accept(Visitor<R> visitor);
+
     interface Visitor<R> {
         R visitExpressionStmt(Expression stmt);
+
         R visitPrintStmt(Print stmt);
+
         R visitVarStmt(Var stmt);
     }
+
     public static class Expression extends Stmt {
+        final Expr expression;
+
         Expression(Expr expression) {
             this.expression = expression;
         }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-        return visitor.visitExpressionStmt(this);
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitExpressionStmt(this);
+        }
     }
 
-        final Expr expression;
-    }
     public static class Print extends Stmt {
+        final Expr expression;
+
         Print(Expr expression) {
             this.expression = expression;
         }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-        return visitor.visitPrintStmt(this);
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitPrintStmt(this);
+        }
     }
 
-        final Expr expression;
-    }
     public static class Var extends Stmt {
+        final Token name;
+        final Expr initializer;
+
         Var(Token name, Expr initializer) {
             this.name = name;
             this.initializer = initializer;
         }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-        return visitor.visitVarStmt(this);
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitVarStmt(this);
+        }
     }
-
-        final Token name;
-        final Expr initializer;
-    }
-
-    abstract <R> R accept(Visitor<R> visitor);
 }
