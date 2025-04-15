037aed933b35aa030904ed4ab387f41aea369e9e
virchich <68477339+virchich@users.noreply.github.com>
2025-04-15 16:50:27 -0400

    chore: Implement improvements around this

--- src/main/java/com/lox/Resolver.java
+++ src/main/java/com/lox/Resolver.java
@@ -98,6 +98,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
         for (Stmt.Function method : stmt.methods) {
             FunctionType declaration = FunctionType.METHOD;
+            if (method.name.lexeme.equals("init")) declaration = FunctionType.INITIALIZER;
             resolveFunction(method, declaration);
         }
 
@@ -141,7 +142,13 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         if (currentFunction == FunctionType.NONE)
             Lox.error(stmt.keyword, "Can't return from top-level code.");
 
-        if (stmt.value != null) resolve(stmt.value);
+        if (stmt.value != null) {
+            if (currentFunction == FunctionType.INITIALIZER)
+                Lox.error(stmt.keyword, "Can't return a value from an initializer.");
+
+            resolve(stmt.value);
+        }
+
         return null;
     }
 
@@ -251,7 +258,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     private enum FunctionType {
-        NONE, FUNCTION, METHOD
+        NONE, FUNCTION, METHOD, INITIALIZER
     }
 
     private enum ClassType {
